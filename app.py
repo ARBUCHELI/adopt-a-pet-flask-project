5c4622067a2d3750b81141b388d9adffe3441caa
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-10 12:15:32 -0500

    adding dog picture

--- app.py
+++ app.py
@@ -7,20 +7,23 @@ def index():
   return '''
     <html style="background-color: #cfe0e8; margin-top: 1em;">
       <h1 style="color: #c94c4c; text-align: center; font-family: 'Courier New', monospace; font-size: 4em; font-weight: bold;">ADOPT A PET!</h1>
-      <img style=" display: block; margin-left: auto; margin-right: auto; border-radius: 5px; border: 1px solid #c94c4c" src="https://raw.githubusercontent.com/ARBUCHELI/adopt-a-pet-flask-project/master/adopt-pet-concept_23-2148517279.avif" alt="Girl in a jacket" width="200" height="200">
-      <h2 style = "text-align: center; color: #034f84; font-family: 'Courier New', monospace; font-size: 1.5em; margin-bottom: 3em;">A minimalistic Flask Web App with information about Pets</h2>
+      <img style=" display: block; margin-left: auto; margin-right: auto; border-radius: 5px; border: 1px solid #c94c4c" src="https://raw.githubusercontent.com/ARBUCHELI/adopt-a-pet-flask-project/master/adopt_a_pet.jpg" alt="Girl in a jacket" width="200" height="200">
+      <h2 style = "text-align: center; color: #034f84; font-family: 'Courier New', monospace; font-size: 1.5em; margin-bottom: 3em;">A minimalistic Pet Adoption Flask Web App</h2>
       <p style = "text-align: center; color: #034f84; font-family: 'Courier New', monospace; font-size: 1.5em;">Browse through the links below to find your new furry friend:</p>
       <ul style = "text-align: center; color: #034f84; font-family: 'Courier New', monospace; font-size: 1.5em; list-style: none;">
         <li style="color: #c94c4c"><a href="/animals/dogs">Dogs</a></li>
         <li style="color: #c94c4c"><a href="/animals/cats">Cats</a></li>
         <li style="color: #c94c4c"><a href="/animals/rabbits">Rabbits</a></li>
       </ul>
+      <a style = "color: #36486b; font-family: 'Courier New', monospace; font-size: 1em; margin-bottom: 3em; position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; font-weight: bold;" href="https://www.freepik.com/free-vector/adopt-pet-concept_8466085.htm#page=2&query=pet%20adoption&position=6&from_view=search&track=ais" target="_blank">Image by pikisuperstar on Freepik</a>
+      <footer style = "color: #c94c4c; font-family: 'Courier New', monospace; font-size: 1.5em; margin-bottom: 3em; position: fixed; left: 0; bottom: 0; width: 100%; text-align: center; font-weight: bold;">Bucheli © Web Development 2023</footer>
     </html>
     '''
 @app.route('/animals/<pet_type>')
 def animals(pet_type):
   html = f'<html style="background-color: #cfe0e8; margin-top: 1em;">'
   html += f'<h1 style="color: #c94c4c; text-align: center; font-family: Courier New, monospace; font-size: 4em; font-weight: bold">List of {pet_type}</h1>'
+  html += f'<img style=" display: block; margin-left: auto; margin-right: auto; border-radius: 5px; border: 1px solid #c94c4c" src="https://raw.githubusercontent.com/ARBUCHELI/adopt-a-pet-flask-project/master/adopt_a_pet.jpg" alt="Girl in a jacket" width="200" height="200">'
   html += '<ul>'
   for idx, item in enumerate(pets[pet_type]):
     html += f'<li><a href="/animals/{pet_type}/{idx}">' + item["name"] +'</a></li>'
